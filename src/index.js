26b30d939b3e8c9273a025a397b5f03a021448df
fwn12tt <fwn12tt>
2023-05-06 14:27:20 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -4,26 +4,27 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const route = require('./routes');
 const app = express();
-  const port = 5000;
+const port = 5000;
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use(
   express.urlencoded({
-        extended: true,
+    extended: true,
   }),
 );
-   app.use(express.json());
+app.use(express.json());
 
 app.use(morgan('combined'));
 
 app.engine(
   'hbs',
-  engine({
-    extname: '.hbs',
-  }),
+          engine({
+            extname: '.hbs',
+          }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', 
+path.join(__dirname, 'views'));
 
 // route
 route(app);
