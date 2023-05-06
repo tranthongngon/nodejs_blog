e0ac8e2209cf709893eb185bd4fdad4fa45bac31
fwn12tt <fwn12tt>
2023-05-06 14:29:39 +0700

    test husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,7 @@
 class SiteController {
-  index(req, res) {
-    res.render('home');
+  index(req, res) {res.render('home');
   }
-  search(req, res) {
+        search(req, res) {
     res.render('search');
   }
 }
--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ app.use(morgan('combined'));
 
 app.engine(
   'hbs',
-          engine({
-            extname: '.hbs',
-          }),
+  engine({
+         extname: '.hbs',
+  }),
 );
-app.set('view engine', 'hbs');
-app.set('views', 
-path.join(__dirname, 'views'));
+app.
+set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 // route
 route(app);
